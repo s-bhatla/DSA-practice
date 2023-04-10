6778692a1ea147397a0c0b76426f4c60c33b2488
Sidharth Bhatla <67830226+s-bhatla@users.noreply.github.com>
2023-04-10 16:41:56 +0530

    Added solution - LeetHub

---
+++ Exceptionally odd - GFG/exceptionally-odd.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        // code here
+        //when we XOR all the numbers, the ones which will occur an even number of times will get cancelled out
+        //As n^n = 0, and 0^n = n
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans = ans^arr[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getOddOccurrence(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
